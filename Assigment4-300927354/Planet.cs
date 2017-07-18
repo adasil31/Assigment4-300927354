d48bf08c3547be49f9f2e736a0a282fdf5e63f35
ttt <ttt@ALE-554RL5K>
2017-07-18 14:58:33 -0400

    Private instance declared

---
+++ Assigment4-300927354/Planet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment4_300927354
+{
+    /// <summary>
+    /// planet class abstract
+    /// </summary>
+    public abstract class Planet
+    {
+        // variable declare first part
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+    }
+}
+
--- Assigment4-300927354/Program.cs
+++ Assigment4-300927354/Program.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Name: Alessandro da Silva Santos
+ *  Date: 2017-07-18
+ *  Description: Assignment 4 Abstract Class
+ *  Version 0.1 - Private instance declared
+ */
+  
 namespace Assigment4_300927354
 {
     class Program
