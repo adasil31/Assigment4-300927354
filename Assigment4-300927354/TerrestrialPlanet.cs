4dfbe4cc7f63363c3f263cc22d787810fef691da
ttt <ttt@ALE-554RL5K>
2017-07-20 10:41:02 -0400

    Terrestrial planet class created

---
+++ Assigment4-300927354/TerrestrialPlanet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment4_300927354
+{
+    class TerrestrialPlanet : Planet, IHasMoons, IHabitable
+    {
+        private bool _oxygen;
+
+        //this is the constructor.
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen) : base(name, diameter, mass)
+        {
+            this._oxygen = oxygen;
+        }
+
+    }
+}
