22cbf3f37089a39c517ead5c344ab2da0ad0a140
ttt <ttt@ALE-554RL5K>
2017-07-22 15:34:41 -0400

    public methods IHasMoons Interface created

---
+++ Assigment4-300927354/IHasMoons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment4_300927354
+{
+    public interface IHasMoons
+    {
+        //public methods IHasMoons Interface created
+        bool HasMoons();
+    }
+}
