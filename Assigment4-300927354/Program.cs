111a06e660bf6e1c9750c3d0b99e7992092a7bc8
ttt <ttt@ALE-554RL5K>
2017-07-22 15:39:43 -0400

    public static WaitForAnyKey Created and main created

--- Assigment4-300927354/Program.cs
+++ Assigment4-300927354/Program.cs
@@ -15,7 +15,24 @@ namespace Assigment4_300927354
     class Program
     {
         static void Main(string[] args)
+
         {
+            //main created
+            GiantPlanet giantPlanet = new GiantPlanet("Saturn", 116464, 5.6836, "Gas");
+            Console.WriteLine(giantPlanet.ToString());
+
+            TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Earth", 12742, 5.972, true);
+            Console.WriteLine(terrestrialPlanet.ToString());
+
+            WaitForAnyKey();
+
+        }
+        // public static WaitForAnyKey Created//
+        public static void WaitForAnyKey()
+        {
+            Console.WriteLine("Are you done? Press any key to close.");
+            Console.ReadKey();
         }
     }
 }
+
