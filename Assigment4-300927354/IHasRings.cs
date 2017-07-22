467c6e0536b2c02ffc5d9097cc2f559d17cf47c5
ttt <ttt@ALE-554RL5K>
2017-07-22 15:36:26 -0400

    public method IhasRings Created

---
+++ Assigment4-300927354/IHasRings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment4_300927354
+{
+    public interface IHasRings
+    {
+        //public method IhasRings Created
+        bool HasRings();
+    }
+}
